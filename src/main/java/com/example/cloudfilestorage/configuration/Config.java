3a02220a3cce1549ae699ca1493130d78889877f
Aitken Bolat <aitken.bolat@gmail.com>
2024-03-31 22:50:08 +0600

    fix:docker

--- src/main/java/com/example/cloudfilestorage/configuration/Config.java
+++ src/main/java/com/example/cloudfilestorage/configuration/Config.java
@@ -5,6 +5,7 @@ import com.example.cloudfilestorage.service.ProjectUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +23,6 @@ public class Config{
     }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
         return http.build();}
 }
