75fb21806181549e095425cfbfaf4b2bcdc3c478
Aitken Bolat <aitken.bolat@gmail.com>
2024-04-05 20:21:49 +0600

    dependencies new

--- src/main/java/com/example/cloudfilestorage/module/User.java
+++ src/main/java/com/example/cloudfilestorage/module/User.java
@@ -1,12 +1,16 @@
 package com.example.cloudfilestorage.module;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
